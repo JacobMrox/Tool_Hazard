9e7aabca771e1318b3514c2ce097d9db0bf2bb24
JacobMrox <therealmrox2@hotmail.com>
2026-01-25 21:42:46 +0000

    Updating games list

--- IntelOrca.Biohazard/BioVersion.cs
+++ IntelOrca.Biohazard/BioVersion.cs
@@ -2,12 +2,25 @@
 {
     public enum BioVersion
     {
-        Biohazard1,
-        Biohazard1_5,
-        Biohazard2,
-        Biohazard3,
-        BiohazardCv,
+        Biohazard1,//Resident Evil 1 (1996) & DualShock Edition (1997)
+        Biohazard1_5,//Resident Evil 2 Prototype
+        Biohazard2,//Resident Evil 2 (1999), DualShock Edition, PC, etc..
+        Biohazard3,//Nemesis (1999), PS1, PC, etc..
         BiohazardSurvivor,//Extra Tool Hazard enum
+        BiohazardCv,//CODE Veronica, Dreamcast, PS2, etc..
+        GunSurvivor2BiohazardCV,//Extra Tool Hazard enum Resident Evil : Survivor 2 Code Veronica
+        BiohazardREmake,//Resident Evil Remake (2002) + HD Remaster (2015)
+        Biohazard0,//Resident Evil 0 (2002) + HD Remaster (2016)
+        GunSurvivor4BiohazardHeroesNeverDie,//Extra Tool Hazard enum Resident Evil : Dead Aim (Gun Survivor 4)
         BiohazardOutbreak,//Extra Tool Hazard enum
+        Biohazard4,//Resident Evil 4 (2005) + HD Remaster (2011)
+        BiohazardDS,//Extra Tool Hazard enum, Deadly Silence (2006)
+        BiohazardUC,//Extra Tool Hazard enum, Umbrella Chronicles
+        Biohazard5,//Resident Evil 5 (2009) + Gold Edition (2010) + HD Remaster (2016)
+        BiohazardDC,//Extra Tool Hazard enum, Darkside Chronicles
+        BiohazardMecenaries3D,//Extra Tool Hazard enum, Mercenaries 3D
+        BiohazardRevelations,//Extra Tool Hazard enum, Revelations 1 3DS + HD Remaster
+        Biohazard6,//Resident Evil 6 (2012)
+        BiohazardRevelations2,//Extra Tool Hazard enum, Revelations 2
     }
 }
